eb41ec5ce087492d8c898d82ecda56b19d177fda
yakkomajuri <38760734+yakkomajuri@users.noreply.github.com>
2018-09-24 15:15:23 +0100

    Create helloworld.sol

---
+++ helloworld.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24; //Versão da linguagem
+
+contract Hello {  //Nome do contrato
+   
+   //Função HelloWorld retorna a mensagem "Hello World"
+    function HelloWorld () public pure returns (string) {  
+          return "Hello World";    
+    }
+}
